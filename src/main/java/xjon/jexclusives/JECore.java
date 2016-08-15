57f005954ae5d1665363ef8bdca483e4583897eb
Jon <elnatan70@gmail.com>
2016-08-15 03:53:10 +0300

    Finishing touches

--- src/main/java/xjon/jexclusives/JECore.java
+++ src/main/java/xjon/jexclusives/JECore.java
@@ -2,13 +2,12 @@ package xjon.jexclusives;
 
 import net.minecraft.init.Blocks;
 import xjon.jexclusives.proxy.CommonProxy;
+import xjon.jexclusives.util.Log;
 import xjon.jexclusives.util.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class JECore
@@ -16,10 +15,11 @@ public class JECore
 	
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
 	public static CommonProxy proxy;
-		
+	
 	@EventHandler
-	public void Init(FMLInitializationEvent event)
+	public void PostInit(FMLPostInitializationEvent event)
 	{
+		Log.info("Loading custom capes...");
 		proxy.InitCapes();
 	}
 	
