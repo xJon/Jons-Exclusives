63389a055321dce3329b0e9a38e529d1a442ab3c
Jon <xJon@users.noreply.github.com>
2022-08-06 18:05:35 +0300

    Bump version

--- src/main/java/xjon/jexclusives/util/Reference.java
+++ src/main/java/xjon/jexclusives/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "jexclusives";
     public static final String MOD_NAME = "Jon's Exclusives";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "xjon.jexclusives.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "xjon.jexclusives.proxy.CommonProxy";
